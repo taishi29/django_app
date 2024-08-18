56656cbca54ea51ce546fa38a366d5115636fafb
taishi <taishi03929@gmail.com>
2024-08-18 22:23:57 +0900

    Hello Django!!と表示

--- django_app/django_app/urls.py
+++ django_app/django_app/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import hello.views as hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', hello.index),
 ]
