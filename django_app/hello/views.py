ed4963de39cefb88f0e5c177488627ebc29b4e9a
taishi <taishi03929@gmail.com>
2024-08-19 01:06:43 +0900

    テンプレートを使って、二つのWEBページを表示できるようにしました。

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    parms={
+    params={
         'title': 'Hello/index',
-        'msg': 'これは、サンプルで作ったページです'
+        'msg': 'これは、サンプルで作ったページです',
+        'goto': 'next',
     }
-    return render(request, 'hello/index.html', parms)
+    return render(request, 'hello/index.html', params)
+
+
+def next(request):
+    params={
+        'title': 'Hello/Next',
+        'msg' : 'これは、もう一つのページです',
+        'goto' : 'index',
+    }
+    return render(request, 'hello/index.html/', params)
+
 # Create your views here.
