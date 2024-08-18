61f18098aae50c0dc9784c9a1950898af330234d
taishi <taishi03929@gmail.com>
2024-08-19 00:51:36 +0900

    テンプレートを作ってみました。

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello Django!!")
+    parms={
+        'title': 'Hello/index',
+        'msg': 'これは、サンプルで作ったページです'
+    }
+    return render(request, 'hello/index.html', parms)
 # Create your views here.
