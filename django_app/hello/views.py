bf1bc3ce4f7fc9247dc06155a93b1bb769f61a8e
taishi29 <115542864+taishi29@users.noreply.github.com>
2024-08-29 00:22:13 +0900

    フォームの作成

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -4,18 +4,19 @@ from django.http import HttpResponse
 def index(request):
     params={
         'title': 'Hello/index',
-        'msg': 'これは、サンプルで作ったページです',
+        'msg': 'あなたの名前は？',
         'goto': 'next',
     }
     return render(request, 'hello/index.html', params)
 
 
-def next(request):
+def form(request):
+    msg = request.POST['msg']
     params={
-        'title': 'Hello/Next',
-        'msg' : 'これは、もう一つのページだよ',
-        'goto' : 'index',
+        'title' : 'Hello/Form',
+        'msg' : 'こんにちは' + msg + 'さん！',
     }
-    return render(request, 'hello/index.html/', params)
+    return render(request, 'hello/index.html', params)
+        
 
 # Create your views here.
