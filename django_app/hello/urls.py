099dc1030077b674bcf2bad0016cb23c6afb1d28
taishi29 <115542864+taishi29@users.noreply.github.com>
2024-08-30 22:55:28 +0900

    ビュー関数のクラス化。TemplateViewクラスを用いることで、POSTとGETの処理をひとまとめできる。

--- django_app/hello/urls.py
+++ django_app/hello/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import HelloView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', HelloView.as_view(), name='index'),
 ]
--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -1,19 +1,30 @@
+from typing import Any
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 from .forms import HelloForm
 
-def index(request):
-    params={
-        'title': 'Hello',
-        'message':'your data',
-        'form':HelloForm()
-    }
-    if (request.method == 'POST'):
-        params['message'] = '名前:' + request.POST['name'] + \
-            '<br>メール:' + request.POST['mail'] + \
-                '<br>年齢:' + request.POST['age'] 
-        params['form'] = HelloForm(request.POST)
-    return render(request, 'hello/index.html', params)
+class HelloView(TemplateView):
+    
+    def __init__(self):
+        self.params = {
+            'title' : 'Hello!',
+            'message' : 'your data:',
+            'form' : HelloForm()
+        }
+    
+    def get(self, request):
+        return render(request, 'hello/index.html', self.params)
+    
+    def post(self, request):
+        msg = 'あなたは、<b>' + request.POST['name'] + \
+            '(' + request.POST['age'] + ')' \
+                '</b>さんです。<br>メールアドレスは<b>' + \
+                    request.POST['mail'] + \
+                        '</b>ですね。'
+        
+        self.params['message'] = msg
+        self.params['form'] = HelloForm(request.POST)
+        return render(request, 'hello/index.html', self.params)
+        
 
-
-# Create your views here.
