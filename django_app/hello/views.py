94a6cf22e750974643e8f89ab2e1a81e97eeee96
taishi29 <115542864+taishi29@users.noreply.github.com>
2024-09-03 23:49:36 +0900

    Friend.objects.all()の中身を確認しました。

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -3,17 +3,9 @@ from .models import Friend
 from .forms import HelloForm
 
 def index(request):
+    data = Friend.objects.all()
     params = {
         'title':'Hello',
-        'message':'all friends.',
-        'form':HelloForm(),
-        'data':[],
+        'data': data,
     }
-    if(request.method == 'POST'):
-        num = request.POST['id']
-        item = Friend.objects.get(id=num)
-        params['data'] = [item]
-        params['form'] = HelloForm(request.POST)
-    else:
-        params['data'] = Friend.objects.all()
     return render(request, 'hello/index.html', params)
