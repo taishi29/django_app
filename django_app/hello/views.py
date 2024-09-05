e3d1ef594d70f7cbd5cd09a6973f5d79aabdcdff
taishi29 <115542864+taishi29@users.noreply.github.com>
2024-09-05 15:47:35 +0900

    Webからデータベースに新しいレコードを登録できるようにしました。しかし、わざわざ、request.POST['指定']でインスタンスを設定しているため、めんどくさいです。。

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -1,33 +1,29 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from .models import Friend
-from django.db.models import QuerySet
+from .forms import HelloForm
 
-
-def __new_str__(self):
-    result = ''
-    for item in self:
-        result += '<tr>'
-        for k in item:
-            result += '<td>' + str(k) + '=' + str(item[k]) + '</td>'
-        result += '</tr>'
-    return result 
-
-QuerySet.__str__ = __new_str__
 
 def index(request):
-    data = Friend.objects.all().values('id', 'name', 'age')
+    data = Friend.objects.all().values()
     params = {
         'title':'Hello',
         'data': data,
     }
     return render(request, 'hello/index.html', params)
 
-'''
-__str__とは、Pythonにおいてオブジェクトを文字列として表示する際に呼び出される特殊メソッドである。
-例えば、print()関数を使ってオブジェクトを出力する場合や、str()関数でオブジェクトを文字列に変換する際に、
-__str__メソッドが自動的に呼ばれる。
-具体的には、print()関数やstr()関数を使ってオブジェクトを文字列として表示しようとすると、
-この__str__メソッドが呼び出され、そのオブジェクトがどのように文字列として表示されるかが定義されている。
-
-このカスタムメソッド__new_str__は、QuerySetオブジェクトをHTMLのテーブル形式で表示しようとしている。
-'''
+def create(request):
+    params = {
+        'title':'Hello',
+        'form': HelloForm(),
+    }
+    if (request.method == 'POST'):
+        name = request.POST['name']
+        mail = request.POST['mail']
+        gender = 'gender' in request.POST
+        age = request.POST['age']
+        birth = request.POST['birthday']
+        friend = Friend(name=name, mail=mail, gender=gender, age=age, birthday=birth)
+        friend.save()
+        return redirect(to='/hello')
+    return render(request, 'hello/create.html', params)
