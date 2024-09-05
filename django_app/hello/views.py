7dde1f2222b7d958c733cfdbd961048e81798d58
taishi29 <115542864+taishi29@users.noreply.github.com>
2024-09-05 11:51:15 +0900

    Friend.object.all()では、Friendインスタンスを簡素に文字列化したものが表示され、レコードの値だけを取り出すことができない。そこで、valueメソッドを用いることで、値だけを取り出すことが可能。

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -3,9 +3,24 @@ from .models import Friend
 from .forms import HelloForm
 
 def index(request):
-    data = Friend.objects.all()
+    data = Friend.objects.all().values()
     params = {
         'title':'Hello',
         'data': data,
     }
     return render(request, 'hello/index.html', params)
+
+'''
+【モデルオブジェクト】
+データベーステーブルの1つのレコード（1行分のデータ）を表します。
+
+【クエリセット】
+テーブルの中の複数のレコードを持った集まり、つまりデータベースから取得された複数のレコードの集合です。
+クエリセットは複数のモデルオブジェクトを含んでいるため、リストのように繰り返し処理したり、個別のレコードにアクセスしたりできます。
+
+しかし、Friend.objects.all()は、Friendインスタンスが表示される。レコード全体が表示されるのではなく、
+表示されるのはクエリセット全体を簡素に文字列化したもの
+
+ここで！
+value()メソッドを使うと、レコードの値だけを取り出すことができる。
+'''
