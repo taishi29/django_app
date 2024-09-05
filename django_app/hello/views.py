76a4e4d9b623ec6e222c7974d300e47ef8916f1f
taishi29 <115542864+taishi29@users.noreply.github.com>
2024-09-05 13:33:59 +0900

    レコード取得のための他のメソッドを扱ってみました。

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -3,7 +3,10 @@ from .models import Friend
 from .forms import HelloForm
 
 def index(request):
-    data = Friend.objects.all().values_list('id', 'name')
+    num = Friend.objects.all().count()
+    first = Friend.objects.all().first()
+    last = Friend.objects.all().last()
+    data = [num, first, last]
     params = {
         'title':'Hello',
         'data': data,
@@ -11,16 +14,8 @@ def index(request):
     return render(request, 'hello/index.html', params)
 
 '''
-【モデルオブジェクト】
-データベーステーブルの1つのレコード（1行分のデータ）を表します。
-
-【クエリセット】
-テーブルの中の複数のレコードを持った集まり、つまりデータベースから取得された複数のレコードの集合です。
-クエリセットは複数のモデルオブジェクトを含んでいるため、リストのように繰り返し処理したり、個別のレコードにアクセスしたりできます。
-
-しかし、Friend.objects.all()は、Friendインスタンスが表示される。レコード全体が表示されるのではなく、
-表示されるのはクエリセット全体を簡素に文字列化したもの
-
-ここで！
-value()メソッドを使うと、レコードの値だけを(キーと値を辞書型として)取り出すことができる。
+all():QuerySetというクラスのインスタンスとしてレコードの値が取り出される。
+count()メソッド:取得したレコードの数を返す
+first()メソッド:allの中の、最初のものだけを返す
+last()メソッド:allの中の、最後のものだけを返す
 '''
